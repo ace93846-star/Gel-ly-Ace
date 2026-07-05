ca09690051181900fc0c65f38376b150437ec10b
ace93846-star <ace93846@gmail.com>
2026-07-05 21:13:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,88 @@
-alert("JS is working!");
-
 function yesAnswer() {
-    alert("YES clicked");
+    document.getElementById("page0").classList.remove("active");
+    document.getElementById("responsePage").classList.add("active");
+
+    document.getElementById("responseTitle").innerText = "Yeheyyy!! 🥳!";
+    document.getElementById("responseMessage").innerText = "It's really Angel! 🌸<br><br>
+
+    I knew it all along. 🤭<br><br>
+
+    Now, let's see what's next...";
+
+    // show button only if YES
+    document.getElementById("continueBtn").style.display = "block";
 }
 
-function noAnswer() {
-    alert("NO clicked");
+
+function noAnswer(){
+
+    alert(
+`Nice try.
+
+I know you're Angel...
+the angel who accidentally landed on Earth. ✨
+
+Now stop lying. 🤭`
+    );
+
+    page0.classList.remove("active");
+
+    page1.classList.add("active");
+
 }
+// =========================
+// ELEMENTS
+// =========================
+
+const envelope = document.getElementById("envelope");
+const photo = document.getElementById("photo");
+const openBtn = document.getElementById("openBtn");
+
+const page0 = document.getElementById("page0");
+const responsePage = document.getElementById("responsePage");
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+
+const responseTitle = document.getElementById("responseTitle");
+const responseMessage = document.getElementById("responseMessage");
+let opened = false;
+
+// =========================
+// OPEN ENVELOPE
+// =========================
+
+function openEnvelope() {
+
+    if (opened) return;
+
+    opened = true;
+
+    // Envelope moves down slightly
+    envelope.classList.add("open");
+
+    // Photo slides up
+    setTimeout(() => {
+        photo.classList.add("show");
+    }, 400);
+
+    // Show button
+    setTimeout(() => {
+        openBtn.style.display = "inline-block";
+    }, 1200);
+
+}
+function goToEnvelope() {
+    document.getElementById("responsePage").classList.remove("active");
+    document.getElementById("page1").classList.add("active");
+}
+// =========================
+// NEXT PAGE
+// =========================
+
+openBtn.addEventListener("click", function () {
+
+    page1.classList.remove("active");
+
+    page2.classList.add("active");
+
+});
